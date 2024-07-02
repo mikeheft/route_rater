00828a923717d9b0ccddea8971b120d72c10147e
Mike Heft <mikeheft@gmail.com>
2024-07-02 16:24:19 -0600

    Add spec for joins table

---
+++ spec/factories/driver_addresses.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+FactoryBot.define do
+  factory :driver_address do
+    driver
+    address
+  end
+end
---
+++ spec/models/driver_address_spec.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe DriverAddress, type: :model do
+  subject { create(:driver_address) }
+  describe "associations" do
+    it { is_expected.to belong_to(:driver) }
+    it { is_expected.to belong_to(:address) }
+
+    it do
+      is_expected.to validate_uniqueness_of(:current)
+        .scoped_to(:driver_id)
+        .with_message("can only have one current Driver")
+    end
+  end
+end
