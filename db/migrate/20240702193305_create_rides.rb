2241626324f9bd4a463d561f6ab8a7e64956a48c
Mike Heft <mikeheft@gmail.com>
2024-07-03 15:46:09 -0600

    remove constraints around null values for rides

--- db/migrate/20240702193305_create_rides.rb
+++ db/migrate/20240702193305_create_rides.rb
@@ -1,9 +1,9 @@
 class CreateRides < ActiveRecord::Migration[7.1]
   def change
     create_table :rides do |t|
-      t.float :duration, index: true, null: false
-      t.float :distance, index: true, null: false
-      t.float :commute_duration, index: true, null: false
+      t.float :duration, index: true
+      t.float :distance, index: true
+      t.float :commute_duration, index: true
       t.monetize :amount
       t.references :driver, null: true, index: true
       t.references :from_address, foreign_key: {to_table: :addresses}, index: true, null: false
