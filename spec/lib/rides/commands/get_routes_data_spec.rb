5064edc9ab3a5a230de3c305a5c88da73f3887f9
Mike Heft <mikeheft@gmail.com>
2024-07-04 11:45:43 -0600

    Work on expecting method call to cache

--- spec/lib/rides/commands/get_routes_data_spec.rb
+++ spec/lib/rides/commands/get_routes_data_spec.rb
@@ -19,4 +19,35 @@ RSpec.describe Rides::Commands::GetRoutesData do
       expect(data.all? { _1.distance_in_meters == 3105 && _1.duration == "577s" })
     end
   end
+
+  it "caches for an hour" do
+    VCR.use_cassette("first_ride_directions") do
+      from_address = create(
+        :address, :with_out_place_id, line_1: "711 Oval Drive", city: "Fort Collins", state: "CO",
+        zip_code: "80521"
+      )
+      to_address = create(
+        :address, :with_out_place_id, line_1: "151 N College Ave", city: "Fort Collins", state: "CO",
+        zip_code: "80524"
+      )
+      create_list(:ride, 2, from_address:, to_address:)
+      rides = Ride.selectable
+      instance = described_class.new
+
+      expect(instance).to receive(:connection).once.and_call_original
+
+      # Call the method under test
+      instance.call(rides:)
+
+      # Mock the `connection` method to expect it not to be called on subsequent call
+      allow(instance).to receive(:connection)
+      expect(instance).to_not receive(:connection)
+
+      # Expect `get_cached_response` to be called on subsequent call
+      expect(instance).to receive(:get_cached_response)
+
+      # Call the method again to trigger caching behavior
+      instance.call(rides:)
+    end
+  end
 end
