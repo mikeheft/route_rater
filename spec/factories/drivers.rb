13afe1668d35e65e3f23171161b055dacf1ec3af
Mike Heft <mikeheft@gmail.com>
2024-07-03 20:45:00 -0600

    Add functionality to get route duration for commuting the rides from address

--- spec/factories/drivers.rb
+++ spec/factories/drivers.rb
@@ -4,5 +4,7 @@ FactoryBot.define do
   factory :driver do
     first_name { Faker::Name.first_name }
     last_name { Faker::Name.last_name }
+
+    association :current_address, factory: :address
   end
 end
