6dd0ebd1ff07ab59499559aeb8eb42ce8f2997fb
Mike Heft <mikeheft@gmail.com>
2024-07-07 16:34:11 -0600

    update seeds

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,16 @@
 # frozen_string_literal: true
 
+# rubocop:disable Lint/RedundantSafeNavigation
 class ApplicationController < ActionController::API
   private def pagination_params
     params.permit(:limit, :offset)
   end
   private def limit
-    pagination_params[:limit] || 2
+    pagination_params[:limit]&.to_i || 2
   end
 
   private def offset
-    pagination_params[:offset] || 0
+    pagination_params[:offset]&.to_i || 0
   end
 end
+# rubocop:enable Lint/RedundantSafeNavigation
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,8 +8,18 @@ ADDRESSES = [
   {line_1:"1107 S Lemay Ave", line_2: "Suite 240", city: "Fort Collins", state: "CO", zip_code: "80524"},
   {line_1:"1024 S Lemay Ave", city: "Fort Collins", state: "CO", zip_code: "80524"},
   {line_1:"4601 Corbett Dr", city: "Fort Collins", state: "CO", zip_code: "80528"},
+  {line_1: "1113 W Plum St", line_2: "Apt 201D", city: "Fort Collins", state: "CO", zip_code: "80512"},
+  {line_1: "3397 WAGON TRAIL RD", city: "Fort Collins", state: "CO", zip_code: "80524"},
+  {line_1: "5700 N HIGHWAY 1", city: "Fort Collins", state: "CO", zip_code: "80524"},
+  {line_1: "6609 DESERT WILLOW WAY", line_2: "Unit 1", city: "Fort Collins", state: "CO", zip_code: "80525"},
+  {line_1: "2002 BATTLECREEK DR", line_2: "APT 6303", city: "Fort Collins", state: "CO", zip_code: "80528"},
+  {line_1: "1901 YORKTOWN AVE", city: "Fort Collins", state: "CO", zip_code: "80526"},
+  {line_1: "3024 SUMAC ST", city: "Fort Collins", state: "CO", zip_code: "80526"},
+  {line_1: "700 E DRAKE RD", line_2: "APT P08", city: "Fort Collins", state: "CO", zip_code: "80525"},
+  {line_1: "333 RIVA RIDGE DR", line_2: "APT C201", city: "Fort Collins", state: "CO", zip_code: "80526"},
 
 ].freeze
+
 
 # Create Drivers
 ActiveRecord::Base.connection.transaction do
@@ -18,14 +28,18 @@ ActiveRecord::Base.connection.transaction do
       first_name = Faker::Name.first_name
       last_name = Faker::Name.last_name
       driver = Driver.build(first_name:,last_name:)
-      address = Address.create(ADDRESSES.sample)
+      address = Address.create(**ADDRESSES.sample)
       driver.driver_addresses.build(address_id: address.id, current: true)
       driver.save!
     end
 
     puts "Creating remaining Addresses..."
     ADDRESSES[(Address.count + 1)..].each do |attrs|
-      Address.create(**attrs)
+      Address.find_or_create_by(line_1: attrs[:line_1], line_2: attrs[:line_2]) do
+        city = attrs[:city]
+        state = attrs[:state]
+        zip_code = attrs[:zip_code]
+      end
     end
 
     puts "Creating Rides..."
