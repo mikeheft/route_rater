107aaa05ff22843f0a72cc4e3d22c1fcc3b26781
Mike Heft <mikeheft@gmail.com>
2024-07-08 11:45:16 -0600

    move pre loading

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -2,10 +2,11 @@
 
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV["RAILS_ENV"] ||= "test"
+Dir[Rails.root.join("app/middleware/*.{rb}")].each { |file| require file }
 require_relative "../config/environment"
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
-Dir[Rails.root.join("app/middleware/*.{rb}")].each { |file| require file }
+
 require "rspec/rails"
 require "spec_helper"
 require "webmock/rspec"
